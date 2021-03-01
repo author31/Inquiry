a13a81936914278efc0b93e949383097ca4eb4e3
Author <tiengtrung30s@gmail.com>
2021-03-01 18:01:28 +0800

    running

--- api/index.js
+++ api/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 
 // Create express instance
 const app = express()
@@ -7,6 +8,7 @@ const app = express()
 const auth = require('./routes/auth')
 
 // Import API Routes
+app.use(cors())
 app.use(express.json());
 app.use(auth)
 
